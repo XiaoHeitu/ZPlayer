df0ac4f507142d489ce59c2ea8a87f4acb8e454a
小黑兔 <173592829@qq.com>
2019-12-10 08:51:59 +0800

    提交代码

---
+++ ZPlayer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog open = new OpenFileDialog();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.LoadFile();
+        }
+
+        public void LoadFile()
+        {
+            var result = open.ShowDialog();
+            if ((result ?? false))
+            {
+
+                this.MyElement.Source = new Uri($"http://localhost:56153/ZFile/Open?file={System.Web.HttpUtility.UrlEncode(open.FileName)}");
+            }
+        }
+    }
+}
---
+++ ZPlayerStreamServer/Controllers/ZFileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZPlayerStreamServer.Controllers
+{
+    public class ZFileController : Controller
+    {
+        [HttpGet]
+        public FileResult Open([FromQuery]string file)
+        {
+            using (FileStream zipToOpen = new FileStream(file, FileMode.Open))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Read))
+                {
+                    return this.File(archive.Entries[0].Open(), "audio/x-wav ");
+                }
+            }
+        }
+    }
+}
