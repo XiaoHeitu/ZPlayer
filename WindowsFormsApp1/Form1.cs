6297157f2e8079dc9eb11a0490552592673a1e09
小黑兔 <173592829@qq.com>
2019-12-13 00:44:39 +0800

    r

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Microsoft.DirectX.AudioVideoPlayback.Video video = Microsoft.DirectX.AudioVideoPlayback.Video.FromUrl(new Uri("http://localhost:20813/ZFile/Open?file=d:\\1.zip"));
+            video.Owner = this.panel1;
+            video.Play();
+        }
+    }
+}
--- ZPlayer/MainWindow.xaml.cs
+++ ZPlayer/MainWindow.xaml.cs
@@ -30,12 +30,14 @@ namespace ZPlayer
 
         public void LoadFile()
         {
-            var result = open.ShowDialog();
-            if ((result ?? false))
-            {
-                this.MyElement.Source = new Uri($"http://localhost:20813/ZFile/Open?file={System.Web.HttpUtility.UrlEncode(open.FileName)}");
+            Microsoft.DirectX.AudioVideoPlayback.Video video = new Microsoft.DirectX.AudioVideoPlayback.Video(@"I:\测试片\Girls Generation\[MV]Girls_Generation-Gee.ts");
+            
+            //var result = open.ShowDialog();
+            //if ((result ?? false))
+            //{
+            //    this.MyElement.Source = new Uri($"http://localhost:20813/ZFile/Open?file={System.Web.HttpUtility.UrlEncode(open.FileName)}");
                 
-            }
+            //}
         }
 
 
@@ -46,27 +48,27 @@ namespace ZPlayer
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {            
-            this.MyElement.Play();
+            //this.MyElement.Play();
         }
 
         private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            var cur = this.MyElement.Volume;
-            var value = cur + ((double)e.Delta / 10000 * 5);
-            if (value <= 0)
-            {
-                value = 0;
-            }
-            if(value >= 1)
-            {
-                value = 1;
-            }
-            this.MyElement.Volume = value;
+            //var cur = this.MyElement.Volume;
+            //var value = cur + ((double)e.Delta / 10000 * 5);
+            //if (value <= 0)
+            //{
+            //    value = 0;
+            //}
+            //if(value >= 1)
+            //{
+            //    value = 1;
+            //}
+            //this.MyElement.Volume = value;
         }
 
         private void MyElement_MediaFailed(object sender, ExceptionRoutedEventArgs e)
         {
-            MessageBox.Show(e.ErrorException.Message, "出错了", MessageBoxButton.OK, MessageBoxImage.Error);
+            //MessageBox.Show(e.ErrorException.Message, "出错了", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
