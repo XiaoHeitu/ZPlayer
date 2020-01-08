d2ea17829fde1e8143b6b24be68b36b56c4f4120
小黑兔 <173592829@qq.com>
2020-01-08 23:34:58 +0800

    tijiao

--- ZPlayer/App.xaml.cs
+++ ZPlayer/App.xaml.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using ZPlayerStreamHost;
 
 namespace ZPlayer
 {
@@ -18,7 +15,6 @@ namespace ZPlayer
     {
         public App()
         {
-            _ = Program.Main(null);
         }
 
     }
--- ZPlayer/MainWindow.xaml.cs
+++ ZPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using LibVLCSharp.Shared;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,7 +12,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -29,23 +29,33 @@ namespace ZPlayer
             
             this.InitializeComponent();
 
+
+
             var currentAssembly = Assembly.GetEntryAssembly();
             var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
             // Default installation path of VideoLAN.LibVLC.Windows
             var libDirectory = new DirectoryInfo(System.IO.Path.Combine(currentDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
 
-            this.vlcControl1.SourceProvider.CreatePlayer(libDirectory/* pass your player parameters here */);
-            this.vlcControl1.SourceProvider.MediaPlayer.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
+
+            Core.Initialize(libDirectory.FullName);
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            VideoView1.MediaPlayer = _mediaPlayer;
         }
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
 
         public void LoadFile()
         {            
-            //var result = open.ShowDialog();
-            //if ((result ?? false))
-            //{
-            //    this.MyElement.Source = new Uri($"http://localhost:20813/ZFile/Open?file={System.Web.HttpUtility.UrlEncode(open.FileName)}");
-                
-            //}
+            var result = open.ShowDialog();
+            if ((result ?? false))
+            {
+
+                _mediaPlayer.Play(new Media(_libVLC, open.FileName));
+
+            }
         }
 
 
